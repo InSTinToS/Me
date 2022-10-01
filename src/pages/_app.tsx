a643eb1619c7491b6d789176dabeb66638de3cd0
Miguel <miguelandradebarreto2@gmail.com>
2022-10-01 18:06:58 -0300

    :construction: remove splash

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,14 @@
-import { Splash } from '@app/components/templates/Splash'
-
 import { GlobalProvider } from '@app/components/providers/GlobalProvider'
 
 import type { TAppPropsWithLayout } from '@app/types/next.types'
 
-import { useEffect, useState } from 'react'
-
 const MyApp = ({ Component, pageProps }: TAppPropsWithLayout) => {
-  const [loading, setLoading] = useState(true)
-
   const getLayout = Component.getLayout || (page => page)
 
-  useEffect(() => {
-    setLoading(false)
-  }, [])
-
-  return loading ? (
-    <Splash />
-  ) : (
-    getLayout(
-      <GlobalProvider>
-        <Component {...pageProps} />
-      </GlobalProvider>
-    )
+  return getLayout(
+    <GlobalProvider>
+      <Component {...pageProps} />
+    </GlobalProvider>
   )
 }
 
